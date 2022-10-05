13ac45a2a1d579be3122bf99e4ad477f3663b4af
prasetyo32 <112843385+prasetyo32@users.noreply.github.com>
2022-10-05 23:17:58 +0700

    about.php

---
+++ about.php
@@ -0,0 +1,40 @@
+<!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+        <div class="section-title">
+          <h2>About</h2>
+          <p>A collage student who has high curiosity, adds experience, ordinary design when bored, always want to learn everything that becomes curious, usually will always continues.</p>
+        </div>
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Collage Student &amp; Web Developer.</h3>
+            <p class="fst-italic">
+              Still a beginner in building a website, but really want to explore it.
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>29 july 2000</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+62 812 7376 2101</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Kab. palembang,Indonesia</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>22</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Beginner</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>rendiprasetyo0729@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                </ul>
+              </div>
+            </div>
+            <p>
+              yups, that's all about me.  <b>Nice to meet you!</b>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section><!-- End About Section -->

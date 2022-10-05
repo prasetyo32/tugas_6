c9ae823b2ce540456cda22227c1ab4c260d3a931
prasetyo32 <112843385+prasetyo32@users.noreply.github.com>
2022-10-05 23:25:31 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,8 @@
+<!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Selamat datang ,</h1>
+      <p><span class="typed" data-typed-items="to, to My, to My Portofolio"></span></p>
+    </div>
+ </section>
+<!-- End Hero -->
